609939c9eb931c57b0f1fe6a5a16139c20bd18f7
Vraj Patel <vraj.patel.12390@gmail.com>
2025-03-20 23:39:21 -0400

    Add credential logging for debugging

--- backend/src/main/java/com/myeccom/backend/BackendApplication.java
+++ backend/src/main/java/com/myeccom/backend/BackendApplication.java
@@ -1,6 +1,9 @@
 package com.myeccom.backend;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.persistence.Entity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -8,9 +11,15 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class BackendApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(BackendApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@PostConstruct
+	public void logCredentials() {
+		logger.info("DB_USERNAME: {}", System.getenv("DB_USERNAME"));
+		logger.info("DB_PASSWORD: {}", System.getenv("DB_PASSWORD"));
 	}
 
 }
