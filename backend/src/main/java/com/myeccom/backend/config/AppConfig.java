442af5a903be0b3cd12950081678836434aea0ed
Vraj Patel <vraj.patel.12390@gmail.com>
2025-03-21 19:33:15 -0400

    Origin Updated

--- backend/src/main/java/com/myeccom/backend/config/AppConfig.java
+++ backend/src/main/java/com/myeccom/backend/config/AppConfig.java
@@ -42,7 +42,7 @@ public class AppConfig {
                                             @Override
                                             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                                                 CorsConfiguration cfg = new CorsConfiguration();
-                                                cfg.setAllowedOrigins(Collections.singletonList("*"));
+                                                cfg.setAllowedOrigins(Collections.singletonList("https://ecommerce-full-stack-one.vercel.app"));
                                                 cfg.setAllowedMethods(Collections.singletonList("*"));
                                                 cfg.setAllowCredentials(true);
                                                 cfg.setAllowedHeaders(Collections.singletonList("*"));
