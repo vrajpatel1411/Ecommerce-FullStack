d7b7d525f9d1c69b1d062e6397cccc3cbec1bbd0
Vraj Patel <vraj.patel.12390@gmail.com>
2025-03-21 23:07:51 -0400

    Cors updated

---
+++ backend/src/main/java/com/myeccom/backend/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.myeccom.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // Allow all endpoints
+                        .allowedOrigins("https://ecommerce-full-stack-one.vercel.app") // Allow frontend origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
+                        .allowedHeaders("*") // Allow all headers
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
